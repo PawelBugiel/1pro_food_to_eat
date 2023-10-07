f8cae6af2aee1cc3a2d5fd20aab0317723a6f139
pawelbugiel <pawel.bugiel.7@gmail.com>
2023-10-07 23:17:20 +0200

    Add test getProduct(), getAllProducts()

--- src/test/java/com/pawelbugiel/foodToEat/repository/ProductRepositoryTest.java
+++ src/test/java/com/pawelbugiel/foodToEat/repository/ProductRepositoryTest.java
@@ -1,13 +1,19 @@
 package com.pawelbugiel.foodToEat.repository;
 
 import com.pawelbugiel.foodToEat.model.Product;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+* a naming convention is "aClass_aMethod_whatWillBeReturned"
+* */
 
 @AutoConfigureTestDatabase
 @DataJpaTest
@@ -17,11 +23,9 @@ public class ProductRepositoryTest {
     private ProductRepository productRepository;
 
     @Test
-    public void createProductTest() {
-
+    public void productRepository_saveProduct_Product() {
         // Arrange
         LocalDate expiryDate = LocalDate.of(2023, 12, 12);
-
         Product product = Product.ProductBuilder.aProduct()
                 .withName("Water")
                 .withQuantity(11)
@@ -29,12 +33,71 @@ public class ProductRepositoryTest {
                 .build();
 
         // Act
-
         Product savedProduct = productRepository.save(product);
 
         // Assert
+        assertThat(savedProduct).isNotNull();
+        assertThat(savedProduct.getName()).isEqualTo("Water");
+        assertThat(savedProduct.getId()).isGreaterThan(0);
 
-        Assertions.assertThat(savedProduct).isNotNull();
-        Assertions.assertThat(savedProduct.getName()).isEqualTo("Water");
     }
+
+    @Test
+    public void productRepository_getProduct_aProduct() {
+        // Arrange
+        Product tempProduct_1 = (Product.ProductBuilder.aProduct()
+                .withName("Milk")
+                .withQuantity(1)
+                .withExpiryDate(LocalDate.of(2024, 1, 1))
+                .build());
+
+        productRepository.save(tempProduct_1);
+
+        Product tempProduct_2 = (Product.ProductBuilder.aProduct()
+                .withName("Mars")
+                .withQuantity(2)
+                .withExpiryDate(LocalDate.of(2055, 2, 2))
+                .build());
+
+        productRepository.save(tempProduct_2);
+
+        // Act
+                        // uses Optional 
+        Product retreivedProduct = productRepository.findById(tempProduct_2.getId()).orElse(null);
+
+        // Assert
+        assert retreivedProduct != null;
+        assertThat(retreivedProduct.getId()).isEqualTo(2);
+    }
+
+    @Test
+    public void productRepository_getAllProducts_ListOfProducts() {
+
+        // Arrange
+        Product tempProduct = (Product.ProductBuilder.aProduct()
+                .withName("Milk")
+                .withQuantity(1)
+                .withExpiryDate(LocalDate.of(2024, 1, 1))
+                .build());
+
+        productRepository.save(tempProduct);
+
+        tempProduct = (Product.ProductBuilder.aProduct()
+                .withName("Mars")
+                .withQuantity(2)
+                .withExpiryDate(LocalDate.of(2055, 2, 2))
+                .build());
+
+        productRepository.save(tempProduct);
+
+        // Act
+        List<Product> products = productRepository.findAll();
+
+        // Assert
+        assertThat(products).isNotNull();
+        assertThat(products.size()).isEqualTo(2);
+
+    }
+
+
 }
