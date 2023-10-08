67bf3ad8eadc14b07a4b98d7783b476371737055
pawelbugiel <pawel.bugiel.7@gmail.com>
2023-10-08 18:43:52 +0200

    Use @BeforeEach and @DirtiesContext to simplify and clean fom bugs

--- src/test/java/com/pawelbugiel/foodToEat/repository/ProductRepositoryTest.java
+++ src/test/java/com/pawelbugiel/foodToEat/repository/ProductRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.pawelbugiel.foodToEat.repository;
 
 import com.pawelbugiel.foodToEat.model.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,16 +14,34 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /*
-* a naming convention is "aClass_aMethod_whatWillBeReturned"
-* */
+ * a naming convention is "aClass_aMethod_whatWillBeReturned"
+ * */
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+// The above annotation is to avoid a problem with cleaning up the embedded database before running each of test methods.
 @AutoConfigureTestDatabase
 @DataJpaTest
 public class ProductRepositoryTest {
 
     @Autowired
     private ProductRepository productRepository;
-    
+
+    @BeforeEach
+    public void setUp() {
+        Product product_1 = (Product.ProductBuilder.aProduct()
+                .withName("Milk")
+                .withQuantity(1)
+                .withExpiryDate(LocalDate.of(2024, 1, 1))
+                .build());
+
+        Product product_2 = (Product.ProductBuilder.aProduct()
+                .withName("Mars")
+                .withQuantity(2)
+                .withExpiryDate(LocalDate.of(2055, 2, 2))
+                .build());
+
+        productRepository.saveAll(List.of(product_1, product_2));
+    }
 
     @Test
     public void productRepository_saveProduct_Product() {
@@ -38,33 +58,14 @@ public class ProductRepositoryTest {
 
         // Assert
         assertThat(savedProduct).isNotNull();
-        assertThat(savedProduct.getName()).isEqualTo("Water");
         assertThat(savedProduct.getId()).isGreaterThan(0);
-
     }
 
     @Test
     public void productRepository_getProduct_aProduct() {
-        // Arrange
-        Product tempProduct_1 = (Product.ProductBuilder.aProduct()
-                .withName("Milk")
-                .withQuantity(1)
-                .withExpiryDate(LocalDate.of(2024, 1, 1))
-                .build());
-
-        productRepository.save(tempProduct_1);
-
-        Product tempProduct_2 = (Product.ProductBuilder.aProduct()
-                .withName("Mars")
-                .withQuantity(2)
-                .withExpiryDate(LocalDate.of(2055, 2, 2))
-                .build());
-
-        productRepository.save(tempProduct_2);
 
         // Act
-                        // uses Optional
-        Product retreivedProduct = productRepository.findById(tempProduct_2.getId()).orElse(null);
+        Product retreivedProduct = productRepository.findById(2L).orElse(null);
 
         // Assert
         assert retreivedProduct != null;
@@ -74,31 +75,11 @@ public class ProductRepositoryTest {
     @Test
     public void productRepository_getAllProducts_ListOfProducts() {
 
-        // Arrange
-        Product tempProduct = (Product.ProductBuilder.aProduct()
-                .withName("Milk")
-                .withQuantity(1)
-                .withExpiryDate(LocalDate.of(2024, 1, 1))
-                .build());
-
-        productRepository.save(tempProduct);
-
-        tempProduct = (Product.ProductBuilder.aProduct()
-                .withName("Mars")
-                .withQuantity(2)
-                .withExpiryDate(LocalDate.of(2055, 2, 2))
-                .build());
-
-        productRepository.save(tempProduct);
-
         // Act
         List<Product> products = productRepository.findAll();
 
         // Assert
         assertThat(products).isNotNull();
         assertThat(products.size()).isEqualTo(2);
-
     }
-
-
 }
