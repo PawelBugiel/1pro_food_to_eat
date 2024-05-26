238aed1b2c0b15c29db8dd262e4cbbc8ed3cd998
pawelbugiel_LL <pawel.bugiel.7@gmail.com>
2024-05-26 12:41:43 +0200

    Refactor GlobalExceptionHandler.java. Remove unused methods. Change implementations : idExceptionHandler(), productNotFoundExceptionHandler()

--- src/main/java/com/pawelbugiel/foodToEat/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/pawelbugiel/foodToEat/handlers/GlobalExceptionHandler.java
@@ -1,53 +1,26 @@
 package com.pawelbugiel.foodToEat.handlers;
 
-import com.pawelbugiel.foodToEat.exceptions.IdErrorResponse;
 import com.pawelbugiel.foodToEat.exceptions.IdException;
-import com.pawelbugiel.foodToEat.exceptions.PageException;
 import com.pawelbugiel.foodToEat.exceptions.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.sql.SQLIntegrityConstraintViolationException;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IdException.class)
-    public ResponseEntity<IdErrorResponse> idExceptionHandler(IdException exception) {
-        String status = HttpStatus.BAD_REQUEST.toString();
-        String message = "Invalid Id passed";
-        IdErrorResponse idErrorResponse = IdErrorResponse.create(status, message);
-
-        return new ResponseEntity<>(idErrorResponse, HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String idExceptionHandler(IdException ex) {
+        return ex.getMessage();
     }
-    // #q to define other errors response classes - as above
+
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<?> productNotFoundExceptionHandler(ProductNotFoundException productNotFoundException) {
-        return ResponseEntity
-                .badRequest()
-                .body(productNotFoundException.getClass().getSimpleName() + " no such product");
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String productNotFoundExceptionHandler(ProductNotFoundException ex) {
+        return (ex.getMessage());
     }
-
-    @ExceptionHandler(PageException.class)
-    public ResponseEntity<?> pageNotFoundExceptionHandler(PageException pageException){
-        return new ResponseEntity<>(pageException.getClass().getSimpleName() + ". Invalid page value", HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException e){
-
-        String errorMessage = e.getBindingResult().getFieldError().getDefaultMessage();
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<?> handleDateValidationException(SQLIntegrityConstraintViolationException e){
-        return new ResponseEntity<>("Something is wrong with Date object", HttpStatus.CONFLICT);
-    }
-
 
 }
