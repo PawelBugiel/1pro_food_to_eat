102c1d898a8dc5f9b709c405359f48d21812ff28
pawelbugiel_LL <pawel.bugiel.7@gmail.com>
2024-05-26 12:24:13 +0200

    refactor UUID_Converter.java

--- src/main/java/com/pawelbugiel/foodToEat/utilities/UUID_Converter.java
+++ src/main/java/com/pawelbugiel/foodToEat/utilities/UUID_Converter.java
@@ -2,17 +2,15 @@ package com.pawelbugiel.foodToEat.utilities;
 
 import com.pawelbugiel.foodToEat.exceptions.IdException;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public class UUID_Converter {
-    // refactor - to a ternary operator ? or other
-    public static Optional<UUID> convertStringToUUID(String stringUUID) {
-        if (stringUUID == null) return Optional.empty();
+
+    public static UUID convertStringToUUID(String id) {
         try {
-            return Optional.of(UUID.fromString(stringUUID));
+            return UUID.fromString(id);
         } catch (IllegalArgumentException e) {
-            throw new IdException();
+            throw new IdException(id);
         }
     }
 }
