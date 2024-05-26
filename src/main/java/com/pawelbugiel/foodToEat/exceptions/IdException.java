e6a9fb33289e5d4e10b9d10b506e86982be7a78d
pawelbugiel_LL <pawel.bugiel.7@gmail.com>
2024-05-26 12:23:20 +0200

    refactor IdException.java

--- src/main/java/com/pawelbugiel/foodToEat/exceptions/IdException.java
+++ src/main/java/com/pawelbugiel/foodToEat/exceptions/IdException.java
@@ -2,20 +2,8 @@ package com.pawelbugiel.foodToEat.exceptions;
 
 public class IdException extends RuntimeException {
 
-    public IdException() {
+    public IdException(String id) {
+        super("Wrong id format : " + id);
     }
-
-    public IdException(String message) {
-        super(message);
-    }
-
-    public IdException(Throwable cause) {
-        super(cause);
-    }
-
-    public IdException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
 
 }
