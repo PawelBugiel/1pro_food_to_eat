6c6d1016e4f8b0a329026a00f06f7b0e4c41052b
pawelbugiel <pawel.bugiel.7@gmail.com>
2025-03-29 12:47:28 +0100

    Delete old approach validating classes

--- backend/src/main/java/com/pawelbugiel/foodtoeat/validators/CalendarValidator.java
+++
@@ -1,7 +0,0 @@
-package com.pawelbugiel.foodtoeat.validators;
-
-public class CalendarValidator {
-    //"expiryDate": "2024-06-44"
-    // org.springframework.http.converter.HttpMessageNotReadableException:
-
-}
--- backend/src/main/java/com/pawelbugiel/foodtoeat/validators/PartialNameValidator.java
+++
@@ -1,5 +0,0 @@
-package com.pawelbugiel.foodtoeat.validators;
-
-public class PartialNameValidator {
-
-}
