f67931050e5abed74870f04d65f1c741d6540f18
pawelbugiel <pawel.bugiel.7@gmail.com>
2025-05-11 13:32:20 +0200

    BE: 1. ProductMapper.java - fix mapping

--- backend/src/main/java/com/pawelbugiel/foodtoeat/mappers/ProductMapper.java
+++ backend/src/main/java/com/pawelbugiel/foodtoeat/mappers/ProductMapper.java
@@ -4,14 +4,16 @@ import com.pawelbugiel.foodtoeat.dtos.ProductRequest;
 import com.pawelbugiel.foodtoeat.dtos.ProductResponse;
 import com.pawelbugiel.foodtoeat.models.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface ProductMapper {
 
+    @Mapping(target = "id", ignore = true)
     Product toProduct(ProductRequest productRequest);
 
     ProductResponse toProductResponse(Product product);
 
     ProductRequest toProductRequest(Product product);
 }
-
